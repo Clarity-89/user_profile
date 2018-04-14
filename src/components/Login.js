2a3aee4b265e3a7600f9ac6769a0367667ca99d5
Oleksandr Khomenko <homes89@ukr.net>
2018-04-14 22:33:53 +0300

    Handle login form input change

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 
 export default class Login extends Component {
     constructor() {
         super();
+
+        this.state = {
+            username: '',
+            password: ''
+        };
+
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(e, type) {
+        this.setState({[type]: e.target.value})
     }
 
     render() {
+        let {username, password} = this.state;
         return (
             <div className="login">
                 <form className="login__form" noValidate>
@@ -14,15 +25,25 @@ export default class Login extends Component {
                     <div className="login__field login__field--username">
                         <label className="login__label">
                             <i className="fas fa-user"/>
-                            <input className="login__input" name="email" placeholder="Username" value=""
-                                   required="required" type="email"/>
+                            <input className="login__input"
+                                   name="email"
+                                   placeholder="Username"
+                                   value={username}
+                                   type="email"
+                                   onChange={(e) => this.handleInput(e, 'username')}
+                            />
                         </label>
                     </div>
                     <div className="login__field login__field--password">
                         <label className="login__label">
                             <i className="fas fa-key"/>
-                            <input className="login__input" type="password" name="password" placeholder="Password"
-                                   value="" required="required"/>
+                            <input className="login__input"
+                                   type="password"
+                                   name="password"
+                                   placeholder="Password"
+                                   value={password}
+                                   onChange={(e) => this.handleInput(e, 'password')}
+                            />
                         </label>
                         <input className="login__input login__btn" value="Log in" type="submit"/>
                     </div>
